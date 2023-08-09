e911c28627726f3c89953e61a6f4948024f7cfa7
Avi Flombaum <4515+aviflombaum@users.noreply.github.com>
2023-08-09 11:11:56 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# shadcn/docs
+
+A tailwindcss documentation theme based on the shadcn documentation but with no dependencies, including shadcn.
+
+## Usage
+
+`npm install`
+
+`npm watch`
+
+`npm serve`
+
+## Style
+
+The content within the `<article>` tag is purposely styled to be compatible with output from a markdown renderer.
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    './*.html'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
+  ]
+}
